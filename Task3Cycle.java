149cba7cfa1b42d9d27c030be42ff70bc97ebb07
sergeimsg <98679437+sergeimsg@users.noreply.github.com>
2022-08-02 00:20:23 +0300

    Add files via upload

---
+++ Task3Cycle.java
@@ -0,0 +1,23 @@
+package by.academy.les03.hw;
+
+import java.util.Scanner;
+
+/*
+Задание 25
+ */
+public class Task3Cycle {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        int res = 1;
+        while (!n.hasNextInt()){
+            System.out.println("Нужно ввести целое число");
+            n.nextLine();
+        }
+           int x = n.nextInt();
+            for (int i = 1; i <= x; i++) {
+                res = res*i;
+                }
+            System.out.println("Факториал числа  " + x + "  равен = " + res);
+        }
+    }
